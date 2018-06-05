297750a028cef698f920bfb3a7bc50e00e600d2c
inder.patliya <inder.patliya@247.ai>
2018-06-05 18:10:20 +0530

    using npx we use shorthand npx webpack --config config.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("this is the first message");
+console.log(" added the dependencies using npx , npx webpack --config config.js");
+console.log(" this will add the node_module folder and required dev dependencies");
+console.log("however we will add all another dependencies later");
+console.log("normally, we alwasys use ./node_modules/.bin/webpack --config config.js to add the binary(.bin) files locally into our project ")
+console.log("here we will use npx webpack --config config.js, to make our work easier");
